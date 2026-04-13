c6dfe3a79e6ebce9de417cea7c2dbcb2a3b3295c
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2026-04-13 09:23:28 +0100

    Some updates due to new Shiny server

--- scripts/deploy_shiny.sh
+++ scripts/deploy_shiny.sh
@@ -1,1 +1,1 @@
-rsync -rvm shiny/* gjb-shiny-x@rstudio:/homes/gjb-shiny-x/shiny/private/marek_covid_prot_neutrophils
+rsync -rvm shiny/* shiny:/home/mgierlinski/ShinyApps/private/covid_prot_neutrophils
